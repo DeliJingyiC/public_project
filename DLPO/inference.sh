4cc7849419030172507bd9d5dc104e53dbc63203
Jingyi Chen <DeliJingyiC@Gmail.com>
2025-02-03 15:49:06 -0500

    update inference

--- DLPO/inference.sh
+++ DLPO/inference.sh
@@ -28,13 +28,12 @@ mkdir -p output/$SLURM_JOB_ID
 WORK_DIR=output/$SLURM_JOB_ID
 
 python --version
-python sh04_inference_finetune.py --checkpoint="/users/PAS2062/delijingyic/project/wavegrad2/checkpoint/wavegrad2_10_01_14_dlpo_epoch=6_loss=-3.660708427429199test.ckpt" --save_audio_dir=$WORK_DIR --resume_from=1059 --filename='DLPO'
+python inference.py --checkpoint="/users/PAS2062/delijingyic/project/wavegrad2/checkpoint/wavegrad2_10_01_14_dlpo_epoch=6_loss=-3.660708427429199test.ckpt" --save_audio_dir=$WORK_DIR --resume_from=1059 --filename='DLPO'
 
-# python sh04_inference_finetune.py --checkpoint="/users/PAS2062/delijingyic/project/wavegrad2/checkpoint/wavegrad2_10_01_02_klinR_999_epoch=33_loss=-3.663050651550293test.ckpt" --save_audio_dir=$WORK_DIR --resume_from=1059 --filename='klinR'
+# python inference.py --checkpoint="/users/PAS2062/delijingyic/project/wavegrad2/checkpoint/wavegrad2_10_01_02_klinR_999_epoch=33_loss=-3.663050651550293test.ckpt" --save_audio_dir=$WORK_DIR --resume_from=1059 --filename='klinR'
 
 # python sh04_baseline_model_1059.py --checkpoint="/users/PAS2062/delijingyic/project/wavegrad2/checkpoint/wavegrad2_07_16_06_epoch=1755.ckpt" --save_audio_dir=$WORK_DIR --resume_from=1755
 
 # python sh04_baseline_model_1059.py --checkpoint="/users/PAS2062/delijingyic/project/wavegrad2/checkpoint/wavegrad2_07_16_06_epoch=1756.ckpt" --save_audio_dir=$WORK_DIR --resume_from=1756
-# srun --account=PAS1957 --ntasks=8 --gpus=1 --pty /bin/bash
 
-# python sh04_inference_finetune.py --checkpoint="/users/PAS2062/delijingyic/project/wavegrad2/checkpoint/wavegrad2_10_27_00_epoch=3.ckpt" --save_audio_dir=$WORK_DIR --resume_from=3
+# python inference.py --checkpoint="/users/PAS2062/delijingyic/project/wavegrad2/checkpoint/wavegrad2_10_27_00_epoch=3.ckpt" --save_audio_dir=$WORK_DIR --resume_from=3
